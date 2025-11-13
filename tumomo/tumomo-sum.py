2729391260351145c8a1b1a4ebd5cba0a6fcd5a4
leanguardia <lean.guardia93@gmail.com>
2025-11-12 22:00:39 -0400

    Refactor product counting: Replace list with dictionary for categories and calculate total products

--- tumomo/tumomo-sum.py
+++ tumomo/tumomo-sum.py
@@ -1,3 +1,22 @@
-products_count = [5, 2, 13, 1, 46, 15, 103, 58, 180, 3, 1, 1, 360, 3, 5, 26, 5]
-total = sum(products_count)
-print(f"The sum is: {total}")
+categorias = {
+    "agropecuaria_y_campo": 5,
+    "alimentos_y_bebidas": 2,
+    "animales_y_mascotas": 13,
+    "arte_y_antiguedades": 1,
+    "autos_y_vehiculos": 48,
+    "deportes_y_fitness": 1,
+    "electronicos": 15,
+    "equipamiento_industrial": 103,
+    "hogar": 37,
+    "inmuebles": 181,
+    "joyas_y_relojes": 3,
+    "materiales_de_construccion": 1,
+    "nautica_y_pesca": 1,
+    "otras_cosas": 360,
+    "restaurantes_y_locales": 3,
+    "salud_y_belleza": 5,
+#   "servicios": 26,  
+    "vestimenta_y_moda": 5
+}
+total_products = sum(categorias.values())
+print(f"The sum is: {total_products}")
